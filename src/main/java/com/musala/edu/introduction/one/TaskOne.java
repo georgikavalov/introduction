2d8a4aff288d88f2b99b8a57acd900463881a198
georgi.kavalov <georgi.kavalov@GeorgiKa-T580.musala.com>
2018-07-09 16:52:20 +0300

    Task 1 initial commit

---
+++ src/main/java/com/musala/edu/introduction/one/TaskOne.java
@@ -0,0 +1,88 @@
+package com.musala.edu.introduction.one;
+
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@code TaskOne} is a static class
+ * 
+ * @author georgi.kavalov
+ *
+ */
+public class TaskOne {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(TaskOne.class);
+
+	/**
+	 * Constructor
+	 */
+	private TaskOne() {
+		throw new IllegalStateException("TaskOne is a static class");
+	}
+
+	/**
+	 * Finds the index of the median of an array.
+	 * 
+	 * @param array
+	 *            of integer values
+	 * @return An integer value representing an index of the array. -1 if not found.
+	 */
+	public static int getIndexOfMedian(int[] array) {
+		int median = getMedianValueBySort(Arrays.copyOf(array, array.length));
+		int medianIndex = findIndexOfValue(array, median);
+		String stringifiedArray = Arrays.toString(array);
+		LOGGER.info("The array {} has a median {} at index {}", stringifiedArray, median, medianIndex);
+		return medianIndex;
+	}
+
+	/**
+	 * Calculates the median of an array.
+	 * 
+	 * @param array
+	 *            An array of integers
+	 * @return The integer value of the median of the array
+	 */
+	private static int getMedianValueBySort(int[] array) {
+		int result = -1;
+		int arrayLength = array.length;
+		Arrays.sort(array);
+		if (arrayLength % 2 != 0) {
+			result = array[arrayLength / 2];
+		} else {
+			result = (array[(arrayLength - 1) / 2] + array[arrayLength / 2]) / 2;
+		}
+		return result;
+	}
+
+	/**
+	 * Finds the index of a value in an array
+	 * 
+	 * @param array
+	 *            The array to be searched.
+	 * @param value
+	 *            The integer value to look for in the array.
+	 * @return The index of the first occurrence of the searched value. -1 if not
+	 *         found.
+	 */
+	private static int findIndexOfValue(int[] array, int value) {
+		int resultIndex = 0;
+		int closest = 0;
+		while (resultIndex < array.length) {
+			if (array[resultIndex] == value) {
+				break;
+			} else if (Math.abs(array[resultIndex] - value) < Math.abs(array[closest] - value)) {
+				closest = resultIndex;
+			}
+			resultIndex++;
+		}
+
+		if (resultIndex == array.length) {
+			String stringifiedArray = Arrays.toString(array);
+			LOGGER.error("Value {} not found in array array {}", value, stringifiedArray);
+			resultIndex = closest;
+		}
+		return resultIndex;
+	}
+}
---
+++ src/test/java/com/musala/edu/introduction/one/TestTaskOne.java
@@ -0,0 +1,54 @@
+package com.musala.edu.introduction.one;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
+
+/**
+ * The {@code TestTaskOne} class contains unit tests for task one of part one
+ * 
+ * @author georgi.kavalov
+ *
+ */
+public class TestTaskOne {
+
+	/**
+	 * Tests getting the median on an array of odd size
+	 */
+	@Test
+	public void testMedianOnOddSizedArray() {
+		final int[] array = new int[] { 5, 4, 4, 1, 2, 0, 3, 9, 8 };
+		assertArrayEquals(new int[] { 1 }, new int[] { TaskOne.getIndexOfMedian(array) });
+	}
+
+	/**
+	 * Tests getting the median on an array of even size
+	 */
+	@Test
+	public void testMedianOnEvenSizedArray() {
+		final int[] array = new int[] { 5, 6, 1, 0, 9, 1 };
+		assertArrayEquals(new int[] { 0 }, new int[] { TaskOne.getIndexOfMedian(array) });
+	}
+
+	/**
+	 * Tests getting the median on an array of same values.
+	 */
+	@Test
+	public void testMedianOnArrayOfSameValues() {
+		final int[] array = new int[] { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 };
+		assertArrayEquals(new int[] { 0 }, new int[] { TaskOne.getIndexOfMedian(array) });
+	}
+
+	/**
+	 * Tests an array of ten random integers.
+	 */
+	@Test
+	public void testMedianOnRandomArray() {
+		int[] array = new int[10];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int) (Math.random() * 10);
+		}
+		assertNotEquals(-1, TaskOne.getIndexOfMedian(array));
+	}
+}
