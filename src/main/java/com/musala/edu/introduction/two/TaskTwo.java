3aa3cc92921113962064182afcf8cd5982a96e03
georgi.kavalov <georgi.kavalov@GeorgiKa-T580.musala.com>
2018-07-12 16:20:27 +0300

    Khadane's algorithm implementation in progress.

--- src/main/java/com/musala/edu/introduction/two/TaskTwo.java
+++ src/main/java/com/musala/edu/introduction/two/TaskTwo.java
@@ -20,10 +20,11 @@ public class TaskTwo {
 	 * @return int[]
 	 */
 	public static int[] getLargestSumSubArrayUsingBruteForce(int[] array) {
-		int highSum = 0;
-		int nextSum = 0;
 		int from = 0;
 		int to = 0;
+
+		int highSum = 0;
+		int nextSum = 0;
 		for (int i = 0; i < array.length; i++) {
 			nextSum = 0;
 			for (int j = i; j < array.length; j++) {
@@ -44,11 +45,18 @@ public class TaskTwo {
 	 * 
 	 * @param array
 	 *            original array of integer values
-	 * @return int[]
+	 * @return A subset of the passed array
 	 */
 	public static int[] getLargestSumSubArrayUsingKhadaneAlgorithm(int[] array) {
 		int from = 0;
-		int to = 0;
+		int to = 1;
+
+		int currentMaxSum;
+		int globalMaxSum;
+		currentMaxSum = globalMaxSum = array[0];
+		for (int i = 1; i < array.length; i++) {
+
+		}
 		return Arrays.copyOfRange(array, from, to);
 	}
 }
